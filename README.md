1f20bef2e992c6bc12be19bde0f125792f24ef6e
RomanGuerra <roman29g@gmail.com>
2023-10-21 05:13:14 -0500

    First major commit

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # CougarThemePark
 Database Management System Web Application
-
-Run this is command line:
-npm install tedious
---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("login-form").addEventListener("submit", 
+    function (e){
+        e.preventDefault();
+        const username = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        if (username === "coug@CTP.com" && password === "123coug") {
+            window.location.href = "dashboard.html";
+        }
+        else {
+            alert("Invalid Credentials");
+        }
+    });
