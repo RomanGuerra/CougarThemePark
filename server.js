22b7ad28e9bb7678b9d476fd8e170c6504b8bce1
vovi-z <81784608+vovi-z@users.noreply.github.com>
2023-10-25 16:33:18 -0500

    Added data.json testing

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 // This file is the main server file for the web application
+const fs = require('fs');
+
 
 // Using Express framework for Node.js
 const express = require('express');
@@ -8,8 +10,12 @@ const port = 3000;
 // Serve static files
 app.use(express.static(__dirname));
 
+app.get("/data", (req, res) => {
+  res.json(fs.readFileSync('data.json'));
+})
+
 // Start the listening on specified port
 console.log(__dirname)
 app.listen(port, () => {
     console.log(`Web app listening at http://localhost:${port}`)
-  });
+});
