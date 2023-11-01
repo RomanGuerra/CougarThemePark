0c5611622ef5e1a76ba674f88c60011d9791e098
vovi-z <81784608+vovi-z@users.noreply.github.com>
2023-11-01 12:28:36 -0500

    Change to one DB connection in WEBAPP.js

--- WEBAPP.js
+++ WEBAPP.js
@@ -58,58 +58,6 @@ function executeStatement(sql, callback) {
     connection.execSql(request);
 }
 
-var Connection = require('tedious').Connection;  
-var config = {  
-    server: 'cougar-park.database.windows.net',  //update me
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'team8', //update me
-            password: 'UHcougar8'  //update me
-        }
-    },
-    options: {
-        // If you are on Microsoft Azure, you need encryption:
-        encrypt: true,
-        database: 'Cougar Theme Park'  //update me
-    }
-};  
-
-var connection = new Connection(config);  
-connection.on('connect', function(err) {  
-    // If no error, then good to proceed.  
-    if (!err) {
-      console.log("Connected");  
-    } else {
-      console.log(err);
-    }
-});  
-
-connection.connect();
-
-var Request = require('tedious').Request;  
-var TYPES = require('tedious').TYPES;  
-
-function executeStatement(sql, callback) {  
-    var request = new Request(sql, (e, r, row) => {});
-
-    _rows = [];
-
-    request.on("row", columns => {
-      var _item = {};
-      for (var name in columns) {
-        _item[name] = columns[name].value;
-      }
-      _rows.push(_item);
-    });
-
-    request.on("doneProc", (rowCount, more, rows) => {
-      callback(_rows);
-    });
-
-    connection.execSql(request);
-}
-
 // Serve static files
 app.use(express.static(__dirname));
 
